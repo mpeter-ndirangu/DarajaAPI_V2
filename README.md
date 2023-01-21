e8271b84103b436750befbebe9a1ae167f823b68
pitzdev <maishp09@gmail.com>
2023-01-21 22:43:13 +0300

    Initial Setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# DarajaAPI_V2
---
+++ src/test/java/com/daraja/DarajaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daraja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarajaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
